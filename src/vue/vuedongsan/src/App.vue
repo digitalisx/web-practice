d0f305ce30fbc6251388c90f9e94518586e5b909
subin5 <osb0408@ajou.ac.kr>
2022-08-01 13:46:05 +0900

    v-for

--- src/vue/vuedongsan/src/App.vue
+++ src/vue/vuedongsan/src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+
+  <div class="menu">
+    <a v-for="a in 메뉴들" :key="a"> {{ a }} </a>
+  </div>
+
   <div>
-    <h4>{{products[0]}}</h4>
+    <h4 v-for="(p,i) in products" :key="i"> {{ p }} </h4>
     <p> 50만원</p>
   </div>
   <div>
-    <h4>{{products[1]}}</h4>
     <p> 60만원</p>
   </div>
   <div>
-    <h4>{{products[2]}}</h4>
     <p> 70만원</p>
   </div>
 
@@ -21,6 +23,7 @@ export default {
   name: 'App',
   data(){
     return {
+      메뉴들 : ['Home', 'Shop', 'About'],
       products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
     }
   },
@@ -36,6 +39,16 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
