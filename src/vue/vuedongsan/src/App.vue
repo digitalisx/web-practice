fd9f4f47e69752bd6ec1fa9e9badb006d28edef1
subin5 <osb0408@ajou.ac.kr>
2022-08-02 18:22:58 +0900

    event handler & function

--- src/vue/vuedongsan/src/App.vue
+++ src/vue/vuedongsan/src/App.vue
@@ -5,13 +5,18 @@
   </div>
 
   <div>
-    <h4 v-for="(p,i) in products" :key="i"> {{ p }} </h4>
+    <h4> {{products[0]}} </h4>
     <p> 50만원</p>
+    <button @click="increase_1">허위매물신고</button> <span> 신고수 : {{신고수[0]}}</span>
   </div>
   <div>
+    <h4> {{products[1]}} </h4>
     <p> 60만원</p>
+    <button @click="increase_2">허위매물신고</button> <span> 신고수 : {{신고수[1]}}</span>
   </div>
   <div>
+    <h4> {{products[2]}} </h4>
+    <button @click="increase_3">허위매물신고</button> <span> 신고수 : {{신고수[2]}}</span>
     <p> 70만원</p>
   </div>
 
@@ -23,8 +28,20 @@ export default {
   name: 'App',
   data(){
     return {
+      신고수: [0,0,0],
       메뉴들 : ['Home', 'Shop', 'About'],
       products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
+    }
+  },
+  methods : {
+    increase_1(){
+      this.신고수[0] += 1;
+    },
+    increase_2(){
+      this.신고수[1] += 1;
+    },
+    increase_3(){
+      this.신고수[2] += 1;
     }
   },
   components: {
