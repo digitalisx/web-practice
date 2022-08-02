9fc8b04e4f0ec89fda8d8dbb9fdb81a3d7dc84ec
subin5 <osb0408@ajou.ac.kr>
2022-08-02 18:48:36 +0900

    modal window & v-if

--- src/vue/vuedongsan/src/App.vue
+++ src/vue/vuedongsan/src/App.vue
@@ -1,22 +1,34 @@
 <template>
 
+  <div class="black-bg" v-if="모달창열렸니">
+    <div class="white-bg">
+      <h4>상세페이지</h4>
+      <p>내용</p>
+      <button @click="모달창열렸니=false">닫기</button>
+    </div>
+  </div>
+
+
   <div class="menu">
     <a v-for="a in 메뉴들" :key="a"> {{ a }} </a>
   </div>
 
   <div>
-    <h4> {{products[0]}} </h4>
+    <img src="./assets/room0.jpg" class="room-img">
+    <h4 @click="모달창열렸니 = true"> {{products[0]}} </h4>
     <p> 50만원</p>
-    <button @click="increase_1">허위매물신고</button> <span> 신고수 : {{신고수[0]}}</span>
+    <button @click="신고수[0]++">허위매물신고</button> <span> 신고수 : {{신고수[0]}}</span>
   </div>
   <div>
+    <img src="./assets/room1.jpg" class="room-img">
     <h4> {{products[1]}} </h4>
     <p> 60만원</p>
-    <button @click="increase_2">허위매물신고</button> <span> 신고수 : {{신고수[1]}}</span>
+    <button @click="신고수[1]++">허위매물신고</button> <span> 신고수 : {{신고수[1]}}</span>
   </div>
   <div>
+    <img src="./assets/room2.jpg" class="room-img">
     <h4> {{products[2]}} </h4>
-    <button @click="increase_3">허위매물신고</button> <span> 신고수 : {{신고수[2]}}</span>
+    <button @click="신고수[2]++">허위매물신고</button> <span> 신고수 : {{신고수[2]}}</span>
     <p> 70만원</p>
   </div>
 
@@ -28,21 +40,13 @@ export default {
   name: 'App',
   data(){
     return {
+      모달창열렸니 : false,
       신고수: [0,0,0],
       메뉴들 : ['Home', 'Shop', 'About'],
       products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
     }
   },
   methods : {
-    increase_1(){
-      this.신고수[0] += 1;
-    },
-    increase_2(){
-      this.신고수[1] += 1;
-    },
-    increase_3(){
-      this.신고수[2] += 1;
-    }
   },
   components: {
   }
@@ -50,6 +54,29 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0
+}
+div {
+  box-sizing: border-box;
+
+}
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
