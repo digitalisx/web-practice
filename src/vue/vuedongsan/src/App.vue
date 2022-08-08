23aeca4e2cec47218120bb7b2519433b68f90895
subin5 <osb0408@ajou.ac.kr>
2022-08-08 18:15:40 +0900

    fix: vuedongsan feedback

--- src/vue/vuedongsan/src/App.vue
+++ src/vue/vuedongsan/src/App.vue
@@ -41,46 +41,4 @@ export default {
 }
 </script>
 
-<style>
-body {
-  margin: 0
-}
-div {
-  box-sizing: border-box;
-
-}
-.black-bg {
-  width: 100%; height: 100%;
-  background: rgba(0,0,0,0.5);
-  position: fixed; padding: 20px;
-}
-.white-bg {
-  width: 100%; background: white;
-  border-radius: 8px;
-  padding: 20px;
-}
-
-.room-img {
-  width: 100%;
-  margin-top: 40px;
-}
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-.menu {
-  background: darkslateblue;
-  padding: 15px;
-  border-radius: 5px;
-}
-
-.menu a {
-  color: white;
-  padding: 10px;
-}
-</style>
+<style src="./style.css"></style>
