1ffd0afb4851deea9b0564615c68e20c7795c61a
subin5 <osb0408@ajou.ac.kr>
2022-08-24 22:11:22 +0900

    fix: snippets intend

--- src/vue/ajou/src/views/IntroductionView.vue
+++ src/vue/ajou/src/views/IntroductionView.vue
@@ -1,6 +1,21 @@
 <template>
-  <div class="about">
+  <div>
     <h1>This is an introduction page</h1>
     <h3>about lab | professor | members | alumni | honorary faculty</h3>
   </div>
 </template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      sampleData: ''
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
