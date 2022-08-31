c0116891a07a35cd6d19773e875b5aff2615a2a7
subin5 <osb0408@ajou.ac.kr>
2022-08-31 14:00:26 +0900

    feature: Cat

--- src/vue/ajou/src/views/CatView.vue
+++ src/vue/ajou/src/views/CatView.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
+    아주대학교 도서관 삼총사를 소개합니다 !
+    <div class="mokju">
+      <img src="@/assets/cat/mokju1.jpg"/>
+      <img src="@/assets/cat/mokju2.jpg"/>
+      <img src="@/assets/cat/mokju3.jpg"/>
+      <img src="@/assets/cat/mokju4.jpg"/>
+      <img src="@/assets/cat/mokju5.jpg"/>
+      <div class="cat-description">
+        이름 : 대목주
+        <br>특징 : 삼총사 중 유일한 하양이, 뚠뚠이, 게으름뱅이
+      </div>
+    </div>
   </div>
 </template>
 
