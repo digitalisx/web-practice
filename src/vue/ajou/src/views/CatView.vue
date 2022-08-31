74235d79b070db51f5ff229b44a3216f6aca92f7
subin5 <osb0408@ajou.ac.kr>
2022-08-31 16:19:07 +0900

    feature: CatView

--- src/vue/ajou/src/views/CatView.vue
+++ src/vue/ajou/src/views/CatView.vue
@@ -1,15 +1,42 @@
 <template>
   <div>
     아주대학교 도서관 삼총사를 소개합니다 !
-    <div class="mokju">
-      <img src="@/assets/cat/mokju1.jpg"/>
-      <img src="@/assets/cat/mokju2.jpg"/>
-      <img src="@/assets/cat/mokju3.jpg"/>
-      <img src="@/assets/cat/mokju4.jpg"/>
-      <img src="@/assets/cat/mokju5.jpg"/>
-      <div class="cat-description">
-        이름 : 대목주
-        <br>특징 : 삼총사 중 유일한 하양이, 뚠뚠이, 게으름뱅이
+    <div class="cat" id="mokju">
+      <div class="main">
+        <img src="@/assets/cat/mokju_main.jpg"/>
+        <p class="cat-description">
+          이름 : 대목주
+          <br>특징 : 삼총사 중 유일한 하양이, 뚠뚠이, 게으름뱅이
+        </p>
+      </div>
+      <div class="sub">
+        <img v-for="index in 4" :key="index" :src="require(`@/assets/cat/mokju${index}.jpg`)"/>
+      </div>
+    </div>
+
+    <div class="cat" id="ppiyak">
+      <div class="main">
+        <img src="@/assets/cat/ppiyak_main.jpg"/>
+        <p class="cat-description">
+          이름 : 삐약이
+          <br>특징 : 애교쟁이, 각도에 굴하지 않는 귀여움
+        </p>
+      </div>
+      <div class="sub">
+        <img v-for="index in 4" :key="index" :src="require(`@/assets/cat/ppiyak${index}.jpg`)"/>
+      </div>
+    </div>
+
+    <div class="cat" id="hakchi">
+      <div class="main">
+        <img src="@/assets/cat/hakchi_main.jpg"/>
+        <p class="cat-description">
+          이름 : 학치
+          <br>특징 : 근육 고양이, 가끔 냥냥펀치
+        </p>
+      </div>
+      <div class="sub">
+        <img v-for="index in 4" :key="index" :src="require(`@/assets/cat/hakchi${index}.jpg`)"/>
       </div>
     </div>
   </div>
@@ -30,3 +57,24 @@ export default {
   methods: {}
 }
 </script>
+
+<style scoped>
+  .cat{
+    border-top: 1.5px solid rgb(53, 127, 236);
+    padding: 5px;
+  }
+
+  .main {
+    width: 100%;
+    height: 100%;
+  }
+
+  .sub img{
+    width: 25%;
+    height: 25%;
+  }
+
+  .sub {
+    padding: 5px;
+  }
+</style>
