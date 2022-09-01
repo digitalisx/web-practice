8dd1c5f38a364d0c04ee09979bb14ddb092e35f6
subin5 <osb0408@ajou.ac.kr>
2022-09-01 14:03:45 +0900

    feature: Grid college page

--- src/vue/ajou/src/components/DepartmentSelect.vue
+++ src/vue/ajou/src/components/DepartmentSelect.vue
@@ -4,10 +4,14 @@
       <option value=null>학과 선택</option>
       <option :key="major.department" v-for="major in majorList.filter(isCollege)">{{ major.department }}</option>
     </select>
-    <p>
-      <img v-if="selectedDepartment !== null" :src="require(`@/assets/chitoh/${selectedDepartment}.jpg`)">
-    </p>
-    <p v-if="description !== null">{{ description }}</p>
+    <div id=grid>
+      <p id="department-image">
+        <img v-if="selectedDepartment !== null" :src="require(`@/assets/chitoh/${selectedDepartment}.jpg`)">
+      </p>
+      <p id="department-description" v-if="description !== null">
+        {{ description }}
+      </p>
+    </div>
   </div>
 </template>
 <script>
@@ -47,7 +51,22 @@ export default {
 
 <style scoped>
   div { margin-top: 20px; }
+
   option {
     font-weight: bold;
+  }
+
+  #grid {
+    display: grid;
+    grid-template-rows: 2fr 1fr;
+    row-gap: 20px;
+  }
+
+  #department-description{
+    font-weight: bold;
+    margin-left: 350px;
+    margin-right: 350px;
+    font-size: 130%;
+    text-align: justify;
   }
 </style>
